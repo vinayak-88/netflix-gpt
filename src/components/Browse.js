47763a95d740e6ed44043ff062d9c6b8455d31f1
Vinayak Goel <vinayak.goel5635@gmail.com>
2024-12-29 08:00:32 +0530

    git push

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,27 @@
 import React, { useEffect } from "react";
-import { Header } from "./Header";
+import { Header } from "./Header"; 
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header />
+      {
+        /*
+         Main Container
+         - VideoBackground
+         - VideoTitle
+        
+        Secondary Container
+         - MovieList * n
+         - MovieCard * n
+        */
+      }
+      <MainContainer/>
+      <SecondaryContainer/>
     </div>
   );
 };
